79a66553d128e1f8bf0f376249cebbb7d64b4961
Hassan Issa <hassan.issa2@outlook.com>
2022-07-13 00:14:00 -0400

    filter / search bar

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ const root = document.querySelector(".student-table");
 
 // Fetch data
 fetch("./students-list.json")
-.then(res => res.json())
-.then(data => data.students)
-.then(displayStudents)
-.catch(err => console.log(err))
+  .then(res => res.json())
+  .then(data => data.students)
+  .then(displayStudents)
+  .catch(err => console.log(err))
 
 // Display Data
 function displayStudents(data) {
-    students = data;
-    let tableHTML = '';
-    students.forEach(student => {
-        tableHTML += 
-        `
+  students = data;
+  let tableHTML = '';
+  students.forEach(student => {
+    tableHTML +=
+      `
         <tr>
                 <td>${student.name}</td>
                 <td>${student.email}</td> 
@@ -23,6 +23,25 @@ function displayStudents(data) {
                 <td>${student.id}</td>
         </tr>
         `
-    });
-    root.innerHTML = tableHTML
+  });
+  root.innerHTML = tableHTML
+}
+
+// Search for student list
+function filterStudent() {
+  let input, filter, table, tr, td, cell, i, j;
+  filter = document.getElementById("searchInput").value.toLowerCase();
+  table = document.getElementById("userTable");
+  tr = table.getElementsByTagName("tr")
+  for (let i = 1; i < tr.length; i++) {
+    tr[i].style.display = "none"
+    const tdArray = tr[i].getElementsByTagName("td")
+    for (let j = 0; j < tdArray.length; j++) {
+      const cellValue = tdArray[j];
+      if (cellValue && cellValue.innerHTML.toLowerCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+        break;
+      }
+    }
+  }
 }
