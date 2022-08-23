6f72a12d5755f69d0b53c1eeb791ec687a1d37ec
Hassan Issa <hassan.issa2@outlook.com>
2022-08-22 20:58:28 -0400

    added csv export feature

--- app.js
+++ app.js
@@ -156,9 +156,11 @@ function removeRowAndFromLocalStorage(event) {
     // Remove the Node from local storage
     let indexOfRowToDelete = array.findIndex((students) => students.name === student.name);
     array.splice(indexOfRowToDelete, 1);
+
     // Remove the number from the grades dropdown filter
     let boolean = checkIfTheSameGradeExist(student.grade);
     boolean ? null: currentOption.splice(indexOfRowToDelete, 1);
+
     // Set local storage and remove node from DOM
     localStorage.setItem("students", JSON.stringify(array));
     event.target.parentElement.remove()
@@ -402,3 +404,48 @@ function checkIfEmailIsUnqiue(value) {
     }
   }
 };
+
+// Gather the table data and join into CSV format
+function tableToCSV() {
+  // Variable to store the final csv data
+  var csv_data = [];
+  // Get each row data
+  var rows = document.getElementsByTagName('tr');
+  for (var i = 0; i < rows.length; i++) {
+      // Get each column data
+      var cols = rows[i].querySelectorAll('td,th');
+      // Stores each csv row data
+      var csvrow = [];
+      for (var j = 0; j < cols.length; j++) {
+          // Get the text data of each cell of
+          // a row and push it to csvrow
+          csvrow.push(cols[j].innerHTML);
+      }
+      // Combine each column value with comma
+      csv_data.push(csvrow.join(","));
+  }
+  // combine each row data with new line character
+  csv_data = csv_data.join('\n');
+  // Call the download function
+  downloadCSVFile(csv_data);
+}
+
+// Function to download the CSV File
+function downloadCSVFile(csv_data) {
+  // Create CSV file object and feed our
+  // csv_data into it
+  CSVFile = new Blob([csv_data], { type: "text/csv" });
+  // Create to temporary link to initiate
+  // download process
+  var temp_link = document.createElement('a');
+  // Download csv file
+  temp_link.download = "Ta.csv";
+  var url = window.URL.createObjectURL(CSVFile);
+  temp_link.href = url;
+  // This link should not be displayed
+  temp_link.style.display = "none";
+  document.body.appendChild(temp_link);
+  // Automatically click the link to trigger download
+  temp_link.click();
+  document.body.removeChild(temp_link);
+}
